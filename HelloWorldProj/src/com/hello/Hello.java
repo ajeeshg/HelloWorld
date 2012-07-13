97a52c3234eb73dc3837d1c25d5ce3213ac45917
Ajeesh <naira@campusad.msu.edu>
2012-07-13 09:16:29 -0400

    Modified Hello Main

--- HelloWorldProj/src/com/hello/Hello.java
+++ HelloWorldProj/src/com/hello/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello");
+		System.out.println("Hello World");
 	}
 
 }
